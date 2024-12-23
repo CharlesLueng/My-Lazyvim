ab4ae02137cf8cc259bef87b1d9695b492fb596d
CharlesLueng <chaohui_liang@diti365.com>
2024-12-24 00:34:19 +0800

    修改Window命令行为powershell

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,36 @@
 -- Add any additional options here
 
 vim.g.snacks_animate = false
+
+-- Terminal
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  -- -- 命令行重置为powershell
+  local powershell_options = {
+    shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
+
+  for option, value in pairs(powershell_options) do
+    vim.opt[option] = value
+  end
+  -- vim.opt.shell = "nu"
+
+  -- local nushell_options = {
+  --   shell = "nu",
+  --   shellcmdflag = "--login --stdin --no-newline -c",
+  --   shellredir = "out+err> %s",
+  --   shellpipe = "| complete | update stderr { ansi strip } | tee { get stderr | save --force --raw %s } | into record",
+  --   shelltemp = false,
+  --   shellxescape = "",
+  --   shellxquote = "",
+  --   shellquote = "",
+  -- }
+  --
+  -- for option, value in pairs(nushell_options) do
+  --   vim.opt[option] = value
+  -- end
+end
